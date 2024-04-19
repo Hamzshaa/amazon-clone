17ce7e8aabeb1fcd11f44401359da87f5016736d
Hamzshaa <trident32000@gmail.com>
2024-04-20 01:26:30 +0300

    carousel effect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import CarouselComponent from "./components/Carousel/CarouselComponent";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="p-0 m-0">
       <Header />
+      <CarouselComponent />
     </div>
   );
 }
