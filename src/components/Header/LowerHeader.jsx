61fe9a3a6b5380a5e97a561e4e06eb5740ba4fa8
Hamzshaa <trident32000@gmail.com>
2024-04-20 00:23:31 +0300

    Lower Header added

--- src/components/Header/LowerHeader.jsx
+++ src/components/Header/LowerHeader.jsx
@@ -1,3 +1,25 @@
+import { AiOutlineMenu } from "react-icons/ai";
+
 export default function LowerHeader() {
-  return <div className=""></div>;
+  return (
+    <div className="bg-[#232f3e] text-white">
+      <ul className="flex list-none gap-[5px]">
+        <li className="p-2 hover:border-white hover:rounded-[3px] flex gap-0.5 items-center">
+          <AiOutlineMenu />
+          All
+        </li>
+        <li className="p-2 hover:border-white hover:rounded-[3px]">
+          Today&apos;s Deals
+        </li>
+        <li className="p-2 hover:border-white hover:rounded-[3px]">
+          Customer Service
+        </li>
+        <li className="p-2 hover:border-white hover:rounded-[3px]">Registry</li>
+        <li className="p-2 hover:border-white hover:rounded-[3px]">
+          Gift Cards
+        </li>
+        <li className="p-2 hover:border-white hover:rounded-[3px]">Sell</li>
+      </ul>
+    </div>
+  );
 }
