3c5525d9a24568e534fb6eb7285711998594e0dc
Hamzshaa <trident32000@gmail.com>
2024-04-23 11:27:04 +0300

    backend added

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+
+dotenv.config();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+
+    res.status(201).send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(400).send("Invalid total amount");
+  }
+});
+
+app.listen(5000, () => console.log("Server is running on port 5000"));
--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -6,6 +6,10 @@ import Payment from "./pages/Payment";
 import Auth from "./pages/Auth";
 import Results from "./pages/Results";
 import ProductDetail from "./pages/ProductDetail";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 export default function Router() {
   return (
@@ -13,7 +17,14 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
